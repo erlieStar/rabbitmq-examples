d43cc45a33b1abb9bf83e7d394a404da00c07274
erlie <290059742@qq.com>
2019-12-22 23:39:56 +0800

    修改quickstart的api

--- src/main/java/com/javashitang/rabbitmq/chapter_1/QuickStartProducer.java
+++ src/main/java/com/javashitang/rabbitmq/chapter_1/QuickStartProducer.java
@@ -1,10 +1,13 @@
 package com.javashitang.rabbitmq.chapter_1;
 
+import com.rabbitmq.client.BuiltinExchangeType;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
-public class Producer {
+public class QuickStartProducer {
+
+    public final static String EXCHANGE_NAME  = "direct_logs";
 
     public static void main(String[] args) throws Exception {
 
@@ -20,13 +23,16 @@ public class Producer {
         // 3.通过connection创建一个channel
         Channel channel = connection.createChannel();
 
-        // 4.通过channel发送数据
+        // 4.创建交换机
+        channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
+
+        // 5.通过channel发送数据
         for (int i = 0; i < 5; i++) {
             String msg = "hello RabbitMQ";
-            channel.basicPublish("", "test001", null, msg.getBytes());
+            channel.basicPublish(EXCHANGE_NAME, "test001", null, msg.getBytes());
         }
 
-        // 5.记得要关闭相关的连接
+        // 6.记得要关闭相关的连接
         channel.close();
         connection.close();
     }
